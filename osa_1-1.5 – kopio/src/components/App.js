d1f957c554ac94600d4e55fdb3d87ce65f3b630b
severi.tikkanen <severitikkanen@gmail.com>
2019-05-08 11:16:38 +0300

    2.4 ja 2.5 Suoritettu

--- osa_1-1.5 – kopio/src/components/App.js
+++ osa_1-1.5 – kopio/src/components/App.js
@@ -2,29 +2,51 @@ import React from "react";
 import Course from "./Course";
 
 const App = () => {
-  const course = {
-    name: "Half Stack -sovelluskehitys",
-    parts: [
-      {
-        name: "Reactin perusteet",
-        exercises: 12,
-        id: 1
-      },
-      {
-        name: "Tiedonvälitys propseilla",
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: "Komponenttien tila",
-        exercises: 14,
-        id: 3
-      }
-    ]
-  };
+  const course = [
+    {
+      name: "Half Stack -sovelluskehitys",
+      id: 1,
+      parts: [
+        {
+          name: "Reactin perusteet",
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: "Tiedonvälitys propseilla",
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: "Komponenttien tila",
+          exercises: 14,
+          id: 3
+        }
+      ]
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: "Middlewaret",
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ];
+
   return (
     <div>
-      <Course course={course} />
+      {course.map(course => (
+        <Course key={course.id} course={course} />
+      ))}
     </div>
   );
 };
