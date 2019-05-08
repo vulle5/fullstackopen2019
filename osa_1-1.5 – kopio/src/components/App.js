ab4fa68a99b357d7e4e737c9253d6be808a063f4
severi.tikkanen <severitikkanen@gmail.com>
2019-05-08 10:46:31 +0300

    2.1 Suoritettu

---
+++ osa_1-1.5 – kopio/src/components/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Course from "./Course";
+
+const App = () => {
+  const course = {
+    name: "Half Stack -sovelluskehitys",
+    parts: [
+      {
+        name: "Reactin perusteet",
+        exercises: 12,
+        id: 1
+      },
+      {
+        name: "Tiedonvälitys propseilla",
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: "Komponenttien tila",
+        exercises: 14,
+        id: 3
+      }
+    ]
+  };
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  );
+};
+
+export default App;
