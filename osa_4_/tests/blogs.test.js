9b0d7a069226e1d136f203032cf136412fc4cde3
severi.tikkanen <severitikkanen@gmail.com>
2019-05-18 17:07:39 +0300

    4.4 Suoritettu

--- osa_4_/tests/blogs.test.js
+++ osa_4_/tests/blogs.test.js
@@ -6,3 +6,27 @@ test("dummy should return one", () => {
   const result = listHelper.dummy(blogs);
   expect(result).toBe(1);
 });
+
+describe("total likes", () => {
+  test("should count sum of blogs", () => {
+    const blogs = [
+      {
+        title: "Java",
+        author: "Minä",
+        url: "google.com",
+        likes: 5,
+        id: "5cdffe8ca0e2862fac5c35b7"
+      },
+      {
+        title: "JavaScript",
+        author: "Minä",
+        url: "google.com",
+        likes: 5,
+        id: "5ce00abea7efa24114584dfb"
+      }
+    ];
+
+    const result = listHelper.totalLikes(blogs);
+    expect(result).toBe(10);
+  });
+});
--- osa_4_/utils/list_helper.js
+++ osa_4_/utils/list_helper.js
@@ -1,5 +1,14 @@
 const dummy = blogs => 1;
 
+const totalLikes = blogs => {
+  let sum = 0;
+  blogs.forEach(blog => {
+    sum += blog.likes;
+  });
+  return sum;
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
