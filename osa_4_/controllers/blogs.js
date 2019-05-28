8e3f7a70b63bb95d3f0d705d4aa07f2d8c62df84
severi.tikkanen <severitikkanen@gmail.com>
2019-05-28 16:40:27 +0300

    4.13 ja 4.14 Suoritettu

--- osa_4_/controllers/blogs.js
+++ osa_4_/controllers/blogs.js
@@ -20,4 +20,34 @@ blogRoutes.post("/", (request, response) => {
     });
 });
 
+blogRoutes.delete("/:id", async (request, response) => {
+  const result = await Blog.findByIdAndRemove(request.params.id);
+  try {
+    if (result) {
+      response.status(204).end();
+    } else {
+      response.status(404).end();
+    }
+  } catch (error) {
+    response.status(400).json(error);
+  }
+});
+
+blogRoutes.put("/:id", (request, response) => {
+  const body = request.body;
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  };
+
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then(updatedBlog => {
+      response.json(updatedBlog.toJSON());
+    })
+    .catch(error => console.log(error));
+});
+
 module.exports = blogRoutes;
