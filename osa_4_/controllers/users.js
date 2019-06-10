18bd784929f732ba0cc7b98b78ffe980d8d151a0
severi.tikkanen <severitikkanen@gmail.com>
2019-06-10 14:38:51 +0300

    4.16 Suoritettu 4.15 korjattu

--- osa_4_/controllers/users.js
+++ osa_4_/controllers/users.js
@@ -2,6 +2,12 @@ const bcrypt = require("bcryptjs");
 const usersRouter = require("express").Router();
 const User = require("../models/User");
 
+usersRouter.get("/", async (request, response) => {
+  User.find({}).then(user => {
+    response.json(user);
+  });
+});
+
 usersRouter.post("/", async (request, response) => {
   try {
     const body = request.body;
@@ -9,15 +15,20 @@ usersRouter.post("/", async (request, response) => {
     const saltRounds = 10;
     const passwordHash = await bcrypt.hashSync(body.password, saltRounds);
 
-    const user = new User({
-      username: body.username,
-      name: body.name,
-      passwordHash
-    });
+    if (body.username.length >= 3 && body.password.length >= 3) {
+      const user = new User({
+        username: body.username,
+        name: body.name,
+        passwordHash
+      });
+      const savedUser = await user.save();
 
-    const savedUser = await user.save();
-
-    response.json(savedUser);
+      response.json(savedUser);
+    } else {
+      response.status(400).json({
+        error: "username and password must be 3 letters or more"
+      });
+    }
   } catch (err) {
     console.log(err);
   }
