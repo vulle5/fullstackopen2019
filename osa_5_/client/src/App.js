d81bb50059375c4d0e066cea75b84d8639a7cd16
severi.tikkanen <severitikkanen@gmail.com>
2019-06-12 14:58:15 +0300

    5.2 Suoritettu

--- osa_5_/client/src/App.js
+++ osa_5_/client/src/App.js
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import loginService from "./services/login";
 import BlogList from "./components/BlogList";
+import LogoutButton from "./components/LogoutButton";
 
 function App() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogAppUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+    }
+  }, []);
 
   const handleLogin = async event => {
     event.preventDefault();
@@ -15,6 +24,7 @@ function App() {
         password
       });
 
+      window.localStorage.setItem("loggedBlogAppUser", JSON.stringify(user));
       setUser(user);
       setUsername("");
       setPassword("");
@@ -53,9 +63,15 @@ function App() {
     );
   };
 
+  const handleLogout = () => {
+    window.localStorage.removeItem("loggedBlogAppUser");
+    setUser(null);
+  };
+
   return (
     <div className="App">
       <h2>{user === null ? "login to application" : "blogs"}</h2>
+      {user !== null && <LogoutButton onLogout={handleLogout} />}
       <p>{user !== null && `${user.name} logged in`}</p>
       {user === null ? loginForm() : <BlogList />}
     </div>
---
+++ osa_5_/client/src/components/LogoutButton.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LogoutButton = ({ onLogout }) => {
+  return <button onClick={onLogout}>logout</button>;
+};
+
+export default LogoutButton;
