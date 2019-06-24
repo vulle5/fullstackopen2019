b4d2df7f8e5973b7793dd95763e4201c3589fe3e
severi.tikkanen <severitikkanen@gmail.com>
2019-06-24 13:42:51 +0300

    6.3 Suoritettu

--- osa_6.3-/src/reducers/anecdoteReducer.js
+++ osa_6.3-/src/reducers/anecdoteReducer.js
@@ -1,29 +1,49 @@
 const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+  "If it hurts, do it more often",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it."
+];
 
-const getId = () => (100000 * Math.random()).toFixed(0)
+const getId = () => (100000 * Math.random()).toFixed(0);
 
-const asObject = (anecdote) => {
+const asObject = anecdote => {
   return {
     content: anecdote,
     id: getId(),
     votes: 0
-  }
-}
+  };
+};
 
-const initialState = anecdotesAtStart.map(asObject)
+export const incrementVote = id => {
+  return {
+    type: "INCREMENT",
+    data: { id }
+  };
+};
+
+const initialState = anecdotesAtStart.map(asObject);
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+  console.log("state now: ", state);
+  console.log("action", action);
 
-  return state
-}
+  switch (action.type) {
+    case "INCREMENT":
+      const id = action.data.id;
+      const anecdoteToVote = state.find(a => a.id === id);
+      const changedAnecdote = {
+        ...anecdoteToVote,
+        votes: anecdoteToVote.votes + 1
+      };
+      return state.map(anecdote =>
+        anecdote.id !== id ? anecdote : changedAnecdote
+      );
+    default:
+      return state;
+  }
+};
 
-export default reducer
+export default reducer;
