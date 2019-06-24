c78dbe7e97e08c6cedf1925b57418eec986b2090
severi.tikkanen <severitikkanen@gmail.com>
2019-06-24 19:28:39 +0300

    6.16 Suoritettu

--- osa_6.3-/src/App.js
+++ osa_6.3-/src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import anecdoteService from "./services/anecdotes";
+import { initializeAnecdotes } from "./reducers/anecdoteReducer";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Notification from "./components/Notification";
 import Filter from "./components/Filter";
 
-const App = () => {
+const App = props => {
+  useEffect(() => {
+    anecdoteService.getAll().then(notes => props.initializeAnecdotes(notes));
+  }, [props]);
+
   return (
     <div>
       <h2>Anecdotes</h2>
@@ -16,4 +23,7 @@ const App = () => {
   );
 };
 
-export default App;
+export default connect(
+  null,
+  { initializeAnecdotes }
+)(App);
