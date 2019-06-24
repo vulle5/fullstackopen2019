956aa609979509c51eac7bf6ecdeef8d04146f54
severi.tikkanen <severitikkanen@gmail.com>
2019-06-24 13:10:00 +0300

    6.2 Suoritettu

--- osa_6_/src/index.js
+++ osa_6_/src/index.js
@@ -1,33 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import reducer from './reducer'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import reducer from "./reducer";
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
 const App = () => {
-  const good = () => {
+  const dispatchClickAction = type => {
     store.dispatch({
-      type: 'GOOD'
-    })
-  }
+      type: `${type}`
+    });
+  };
 
   return (
     <div>
-      <button onClick={good}>hyvä</button> 
-      <button>neutraali</button> 
-      <button>huono</button>
-      <button>nollaa tilastot</button>
+      <button onClick={() => dispatchClickAction("GOOD")}>hyvä</button>
+      <button onClick={() => dispatchClickAction("OK")}>neutraali</button>
+      <button onClick={() => dispatchClickAction("BAD")}>huono</button>
+      <button onClick={() => dispatchClickAction("ZERO")}>
+        nollaa tilastot
+      </button>
       <div>hyvä {store.getState().good}</div>
-      <div>neutraali</div>
-      <div>huono</div>
+      <div>neutraali {store.getState().ok}</div>
+      <div>huono {store.getState().bad}</div>
     </div>
-  )
-}
+  );
+};
 
 const renderApp = () => {
-  ReactDOM.render(<App />, document.getElementById('root'))
-}
+  ReactDOM.render(<App />, document.getElementById("root"));
+};
 
-renderApp()
-store.subscribe(renderApp)
+renderApp();
+store.subscribe(renderApp);
