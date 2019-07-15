20ecb7aa31a50d71e70b85c7cbb58f7d60159d7d
severi.tikkanen <severitikkanen@gmail.com>
2019-07-15 14:14:11 +0300

    CRA ejected

--- osa_7.4-7.20_blog_laajennus/client/.eslintrc.js
+++ osa_7.4-7.20_blog_laajennus/client/.eslintrc.js
@@ -4,6 +4,11 @@ module.exports = {
     es6: true,
     'jest/globals': true
   },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
   extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
     ecmaFeatures: {
