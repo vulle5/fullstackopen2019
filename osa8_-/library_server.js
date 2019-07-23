38fab2195a732ebe88f2788ebaa1a6e2f42bf5ae
severi.tikkanen <severitikkanen@gmail.com>
2019-07-23 11:15:07 +0300

    8.2 Suoritettu

--- osa8_-/library_server.js
+++ osa8_-/library_server.js
@@ -85,16 +85,25 @@ let books = [
 ];
 
 const typeDefs = gql`
+  type Books {
+    title: String!
+    author: String!
+    published: Int
+    genres: [String]
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Books!]!
   }
 `;
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 };
 
